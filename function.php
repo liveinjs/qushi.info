ed7096fd93d49e899fce40ee16824f5d5b46be10
Wang Liang <4141095@qq.com>
2019-01-20 12:46:44 +0800

    Update function.php

--- function.php
+++ function.php
@@ -7,7 +7,7 @@ if(is_single()){
   $img = current(get_attached_media('image'))->guid;
   if(empty($img)) $img = 'https://默认图片';
   $pubDate = get_the_time("Y-m-d\TH:i:s");
-  $xzh = '<script type="application/ld+json">{"@context": "https://ziyuan.baidu.com/contexts/cambrian.jsonld","@id": '.$id.',"appid": "1614111683884403","title": '.$title.',"images": ['.$img.'], "pubDate": '.$pubDate.'}</script>';
+  $xzh = '<script src="//msite.baidu.com/sdk/c.js?appid=1614111683884403"></script><script type="application/ld+json">{"@context": "https://ziyuan.baidu.com/contexts/cambrian.jsonld","@id": '.$id.',"appid": "1614111683884403","title": '.$title.',"images": ['.$img.'], "pubDate": '.$pubDate.'}</script>';
   echo "".$xzh;
 }
 ?>
