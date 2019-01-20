4688fe13c041ce0be4525ebb23abc40dd9fabeed
Wang Liang <4141095@qq.com>
2019-01-20 12:52:43 +0800

    Update function.php

--- function.php
+++ function.php
@@ -1,5 +1,5 @@
-<?php 
-//wordpress 熊掌号结构化改造
+<?php
+//wordpress 熊掌号机构化改造
 if(is_single()){
   $id = get_permalink();
   $pid = get_the_id();
@@ -7,7 +7,7 @@ if(is_single()){
   $img = current(get_attached_media('image'))->guid;
   if(empty($img)) $img = 'https://默认图片';
   $pubDate = get_the_time("Y-m-d\TH:i:s");
-  $xzh = '<script src="//msite.baidu.com/sdk/c.js?appid=1614111683884403"></script><script type="application/ld+json">{"@context": "https://ziyuan.baidu.com/contexts/cambrian.jsonld","@id": '.$id.',"appid": "1614111683884403","title": '.$title.',"images": ['.$img.'], "pubDate": '.$pubDate.'}</script>';
-  echo "".$xzh;
+  $xzh = '<script src="//msite.baidu.com/sdk/c.js?appid=1614111683884403"></script><script type="application/ld+json">{"@context": "https://ziyuan.baidu.com/contexts/cambrian.jsonld","@id": "'.$id.'","appid": "1614111683884403","title": "'.$title.'","images": ["'.$img.'"], "pubDate": "'.$pubDate.'"}</script>';
+  echo $xzh;
 }
 ?>
