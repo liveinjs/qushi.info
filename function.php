bf159e51196a78c426ba8594a59f697a7ae9e901
Wang Liang <4141095@qq.com>
2019-01-20 12:39:11 +0800

    Create function.php

---
+++ function.php
@@ -0,0 +1,13 @@
+<?php 
+//wordpress 熊掌号结构化改造
+if(is_single()){
+  $id = get_permalink();
+  $pid = get_the_id();
+  $title = get_the_title($pid);
+  $img = current(get_attached_media('image'))->guid;
+  if(empty($img)) $img = 'https://默认图片';
+  $pubDate = get_the_time("Y-m-d\TH:i:s");
+  $xzh = '<script type="application/ld+json">{"@context": "https://ziyuan.baidu.com/contexts/cambrian.jsonld","@id": '.$id.',"appid": "1614111683884403","title": '.$title.',"images": ['.$img.'], "pubDate": '.$pubDate.'}</script>';
+  echo "".$xzh;
+}
+?>
