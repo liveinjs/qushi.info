955d856e45a425c3b4aac23141c032f154f12500
Wang Liang <4141095@qq.com>
2019-01-23 09:11:37 +0800

    Update function.php

--- function.php
+++ function.php
@@ -10,4 +10,14 @@ if(is_single()){
   $xzh = '<script src="//msite.baidu.com/sdk/c.js?appid=1614111683884403"></script><script type="application/ld+json">{"@context": "https://ziyuan.baidu.com/contexts/cambrian.jsonld","@id": "'.$id.'","appid": "1614111683884403","title": "'.$title.'","images": ["'.$img.'"], "pubDate": "'.$pubDate.'"}</script>';
   echo $xzh;
 }
-?>
+？>
+  
+//用于SEO header.php
+	<?php
+		if(is_front_page()){
+			$keywords = '<meta name="keywords" content="趋势观察站,趋势观察,趋势分析,趋势研究,最新趋势" />';
+			$description = '<meta name="description" content="趋势观察站是专业的趋势观察以及趋势研究与趋势分析的网站,免费的,公开的,持续的分享最新趋势发现." />';
+			echo $keywords."\r\n\t".$description."\r\n" ;
+		}
+	?>
+
